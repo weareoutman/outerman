f1704861f413ed8399c0f79468cae29372aca019
Shenwei Wang <wangshenwei@qq.com>
2014-01-23 18:15:42 +0800

    Replace bust with sha1 of files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,6 @@
 var requirejs = require('requirejs')
   , fs = require('fs')
+  , crypto = require('crypto')
   , confList = ['main', 'style']
   , confPaths = confList.map(function(key){
     return 'tools/' + key + '-build.json';
@@ -10,38 +11,65 @@ module.exports = function(grunt) {
 
   // Replace bust url args
   grunt.registerTask('bust', 'Replace the bust url arg', function(target){
-    console.log('--- bust now');
-    var path = 'views/layout.jade'
-      , text = fs.readFileSync(path).toString('utf8')
-      , reg = new RegExp('\\b(bust=' + target + ')(\\.\\w+)?\\b');
-    if (reg.test(text)) {
-      text = text.replace(reg, '$1.' + Date.now().toString(36));
-      fs.writeFileSync(path, text);
-      grunt.log.ok('Bust of "' + target + '" replaced.');
-    } else {
-      grunt.log.ok('Bust of "' + target + '" not found.');
+    if (! target) {
+      return grunt.fail.fatal('Need a target.');
     }
+    var done = this.async()
+      , path = 'views/layout.jade'
+      , out = confs[confList.indexOf(target)].out
+      , reg = new RegExp('\\b(bust=' + target + ')(?:\\.(\\w+))?\\b');
+    fs.readFile(path, {encoding: 'utf8'}, function(err, text){
+      if (err) {
+        return grunt.fail.fatal(err);
+      }
+      var match = text.match(reg);
+      if (match) {
+        fs.readFile(out, function(err, data){
+          if (err) {
+            return grunt.fail.fatal(err);
+          }
+          var sha1 = crypto.createHash('sha1').update(data).digest('hex').substr(0,10);
+          if (match[2] === sha1) {
+            grunt.log.ok('Bust of "' + target + '" not modified.');
+            return done();
+          }
+          text = text.replace(reg, '$1.' + sha1);
+          fs.writeFile(path, text, function(err){
+            if (err) {
+              return grunt.fail.fatal(err);
+            }
+            grunt.log.ok('Bust of "' + target + '" replaced.');
+            done();
+          });
+        });
+      } else {
+        grunt.log.ok('Bust of "' + target + '" not found.');
+        done();
+      }
+    });
   });
 
   // Run the r.js build script
   grunt.registerTask('build', 'Run the r.js build script', function(target){
+    var conf;
     if (target) {
-      confs = confs[confList.indexOf(target)];
-      if (! confs) {
-        grunt.fail.fatal('Config not found "' + target + '".');
-        return;
+      conf = confs[confList.indexOf(target)];
+      if (! conf) {
+        return grunt.fail.fatal('Config not found "' + target + '".');
       }
-      confs = [confs];
+      conf = [conf];
+    } else {
+      conf = confs;
     }
     var done = this.async()
-      , left = confs.length;
+      , left = conf.length;
     function one() {
       left -= 1;
       if (left <= 0) {
         done();
       }
     }
-    confs.forEach(function(conf, index){
+    conf.forEach(function(conf, index){
       requirejs.optimize(conf, function(out){
         var name = (target || confList[index]);
         grunt.log.writeln(out);
