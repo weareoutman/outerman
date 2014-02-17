ae9c11515746e46cda7d57087b1fc4aa56fa396b
Shenwei Wang <wangshenwei@qq.com>
2014-02-18 00:02:03 +0800

    Log to files, and redirect www to non-www

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 var express = require('express')
+  , fs = require('fs')
   , app = express()
   , main = express()
-  , staticServer = express()
   , user = require('./routes/user')
   , oauth = require('./lib/oauth')
   , article = require('./routes/article')
   , db = require('./lib/db')
-  , conf = require('./config');
+  , conf = require('./config')
+  , mainLog = fs.createWriteStream('./log/main.log', {flags: 'a'})
+  , staticLog = fs.createWriteStream('./log/static.log', {flags: 'a'});
 
 var RedisStore = require('connect-redis')(express);
 
 main.enable('case sensitive routing');
-main.use(express.logger('dev'));
+main.use(express.logger({
+  stream: mainLog
+}));
 main.use(express.compress());
 
 main.use(express.favicon(__dirname + '/public/favicon.ico'));
@@ -95,16 +99,25 @@ main.put('/article/:uri', user.restrict, article.update, function(req, res){
 });
 
 // 静态文件服务
+var staticServer = express();
 staticServer.enable('case sensitive routing');
-staticServer.use(express.logger('dev'));
+staticServer.use(express.logger({
+  stream: staticLog
+}));
 staticServer.use(express.compress());
 staticServer.use(express.static(__dirname + '/public', {
   maxAge: 8.64e7 // 1天
 }));
 
-// app.enable('trust proxy');
+var wwwServer = express();
+wwwServer.all('*', function(req, res){
+  res.redirect(301, 'http://weihub.com' + req.originalUrl);
+});
+
+app.enable('trust proxy');
 app.use(express.vhost('weihub.com', main));
 app.use(express.vhost('c.weihub.com', staticServer));
+app.use(express.vhost('www.weihub.com', wwwServer));
 
 app.listen(conf.port, conf.host);
 console.log('[%s] Express started listen on %s:%s', new Date().toUTCString(), conf.host, conf.port);
